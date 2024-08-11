51ba7d008b03b095bc700ea938b46c90d41405d5
Ajmuha8988 <154301848+Ajmuha8988@users.noreply.github.com>
2024-08-11 20:10:35 +0400

    СППР
    Система Поддержки Принятия Решения(статус : незавершён)

---
+++ KubKha/Services/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using KubKha.Models;
+
+namespace KubKha.Services
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<Boss> Bosses { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<TeamView> TeamViews { get; set; }
+
+        public ApplicationContext() : base("DefaultConnection") 
+        {
+        
+        }
+    }
+}
